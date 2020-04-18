ca0ebc21edcacabd7a2a88c51beb15fbdc0abef6
Adiboubadriss2020 <oubadrissadib55@gmail.com>
2020-04-19 00:19:35 +0100

    First version

--- app/src/main/java/com/example/welcome/MainActivity.java
+++ app/src/main/java/com/example/welcome/MainActivity.java
@@ -1,41 +1,78 @@
 package com.example.welcome;
 
-import android.app.Activity;
+import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.content.IntentFilter;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.BatteryManager;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.SearchView;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity {
-private TextView textView;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+import java.io.IOException;
+import java.util.List;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+    GoogleMap map;
+    Marker m;
+    SupportMapFragment mapFragment;
+    SearchView searchView;
+
+    /* access modifiers changed from: protected */
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = (TextView) findViewById(R.id.textview);
-        TextView abt = (TextView) findViewById(R.id.abt);
-        final Animation animation = AnimationUtils.loadAnimation(this,R.anim.transition);
-        textView.startAnimation(animation);
-       final Intent i = new Intent(this,MapsActivity.class);
-        Thread timer = new Thread(){
-            public void run ()
-            {
-                try {
-                   sleep(5000);
-                }catch (Exception e)
-                {
+        this.searchView = (SearchView) findViewById(R.id.search);
+        this.mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
 
-                }finally {
-                    startActivity(i);
-                  System.exit(0);
-
+        this.searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            public boolean onQueryTextSubmit(String query) {
+                String location = MainActivity.this.searchView.getQuery().toString();
+                List list = null;
+                if (location != null || !location.equals("")) {
+                    try {
+                        list = new Geocoder(MainActivity.this).getFromLocationName(location, 1);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    Address address = (Address) list.get(0);
+                    LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                    m = MainActivity.this.map.addMarker(new MarkerOptions().position(latLng).title(location));
+                    MainActivity.this.map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10.0f));
                 }
+                return false;
             }
-        };
-        timer.start();
+
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        this.mapFragment.getMapAsync(this);
+    }
+
+    public void onMapReady(GoogleMap googleMap) {
+        this.map = googleMap;
     }
 }
+
+
+
